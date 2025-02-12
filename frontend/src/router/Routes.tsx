438d67770a785d71ebf9b8db008275319da89f11
Raphaël Pietrzak <raphael.pietrzak@ynov.com>
2025-02-12 09:53:57 +0100

    routes

--- frontend/src/router/Routes.tsx
+++ frontend/src/router/Routes.tsx
@@ -6,9 +6,6 @@ import MoviesList from '../pages/MoviesList';
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Dashboard from "../pages/backoffice/Dashboard";
-import ManageMovie from "../pages/backoffice/ManageMovie";
-import ManageSession from "../pages/backoffice/ManageSession";
-import ManageUser from "../pages/backoffice/ManageUser";
 import SeatSelectorPage from '../pages/SeatSelectorPage';
 import MovieDetailsPage from '../pages/MovieDetailsPage';
 import AddMovie from "../pages/backoffice/AddMovie.tsx";
@@ -18,38 +15,17 @@ export const routes: RouteObject[] = [
         path: '/',
         element: <App />,
         children: [
-            {
-                path: '/',
-                element: <Home />
-            },
-            {
-                path: '/movies',
-                element: <MoviesList />
-            },
-            {
-                path: '/login',
-                element: <Login />
-            },
-            {
-                path: '/register',
-                element: <Register />
-            },
-            {
-                path: '/backoffice',
-                element: <Dashboard />,
-            },
-            {
-                path: '/seat-selector',
-                element: <SeatSelector />
-            },
-            {
-                path: '/movie-detail',
-                element: <MovieDetailsPage />
-            },
-            {
-                path: '/movie/:id',
-                element: <MovieDetails />
-            },
+            { path: '/', element: <Home /> },
+            { path: '/movies', element: <MoviesList /> },
+            { path: '/login', element: <Login /> },
+            { path: '/register', element: <Register /> },
+            { path: '/backoffice', element: <Dashboard /> },
+            { path: '/backoffice/add-movie', element: <AddMovie /> },
+            // Route pour les détails d'un film, avec filmId en paramètre
+            { path: '/movie-detail/:filmId', element: <MovieDetailsPage /> },
+            // Route pour la sélection de sièges, avec sessionId en paramètre
+            { path: '/seat-selector/:sessionId', element: <SeatSelectorPage /> },
+            { path: '/about', element: <About /> },
         ]
     }
 ];
